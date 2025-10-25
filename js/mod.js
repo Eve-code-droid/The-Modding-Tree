e9a45c5c1dba7a2affcf2969c7e83461f442b096
Eve-code-droid <pythoncoderuser@gmail.com>
2025-10-25 17:27:04 +0100

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -6,8 +6,8 @@ let modInfo = {
 
 	discordName: "",
 	discordLink: "",
-	initialStartPoints: new Decimal (10), // Used for hard resets and new players
-	offlineLimit: 1,  // In hours
+	initialStartPoints: new Decimal(10), // Used for hard resets and new players
+	offlineLimit: 1, // In hours
 }
 
 // Set your version in num and name
@@ -27,30 +27,56 @@ let winText = `Congratulations! You have reached the end and beaten this game, b
 // (The ones here are examples, all official functions are already taken care of)
 var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"]
 
-function getStartPoints(){
-    return new Decimal(modInfo.initialStartPoints)
+function getStartPoints() {
+	return new Decimal(modInfo.initialStartPoints)
 }
 
 // Determines if it should show points/sec
-function canGenPoints(){
+function canGenPoints() {
 	return true
 }
 
-// Calculate points/sec!
+// ============================
+// Calculate points per second!
+// ============================
 function getPointGen() {
-	if(!canGenPoints())
+	if (!canGenPoints())
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
+
+	// === Prestige (p) layer upgrades ===
+	if (hasUpgrade('p', 11)) gain = gain.times(upgradeEffect('p', 11))
+	if (hasUpgrade('p', 12)) gain = gain.times(upgradeEffect('p', 12))
+	if (hasUpgrade('p', 13)) gain = gain.times(upgradeEffect('p', 13))
+	if (hasUpgrade('p', 14)) gain = gain.times(upgradeEffect('p', 14))
+	if (hasUpgrade('p', 15)) gain = gain.times(upgradeEffect('p', 15))
+	if (hasUpgrade('p', 16)) gain = gain.times(upgradeEffect('p', 16))
+	if (hasUpgrade('p', 17)) gain = gain.times(upgradeEffect('p', 17))
+	if (hasUpgrade('p', 18)) gain = gain.times(upgradeEffect('p', 18))
+	if (hasUpgrade('p', 19)) gain = gain.times(upgradeEffect('p', 19))
+	if (hasUpgrade('p', 20)) gain = gain.times(upgradeEffect('p', 20))
+
+	// === Science (s) layer upgrades ===
+	// (Add these later — they will boost points when you add the Science layer)
+	if (hasUpgrade('s', 11)) gain = gain.times(upgradeEffect('s', 11))
+	if (hasUpgrade('s', 12)) gain = gain.times(upgradeEffect('s', 12))
+	if (hasUpgrade('s', 13)) gain = gain.times(upgradeEffect('s', 13))
+	if (hasUpgrade('s', 14)) gain = gain.times(upgradeEffect('s', 14))
+	if (hasUpgrade('s', 15)) gain = gain.times(upgradeEffect('s', 15))
+
 	return gain
 }
 
-// You can add non-layer related variables that should to into "player" and be saved here, along with default values
-function addedPlayerData() { return {
-}}
+// You can add non-layer related variables that should go into "player" and be saved here, along with default values
+function addedPlayerData() {
+	return {
+	}
+}
 
 // Display extra things at the top of the page
 var displayThings = [
+	// Example: () => "Your multiplier is " + format(tmp.p.effect)
 ]
 
 // Determines when the game "ends"
@@ -58,21 +84,20 @@ function isEndgame() {
 	return player.points.gte(new Decimal("e280000000"))
 }
 
-
-
-// Less important things beyond this point!
+// ============================
+// Less important things below
+// ============================
 
 // Style for the background, can be a function
 var backgroundStyle = {
-
 }
 
 // You can change this if you have things that can be messed up by long tick lengths
 function maxTickLength() {
-	return(3600) // Default is 1 hour which is just arbitrarily large
+	return (3600) // Default is 1 hour which is just arbitrarily large
 }
 
 // Use this if you need to undo inflation from an older version. If the version is older than the version that fixed the issue,
 // you can cap their current resources with this.
-function fixOldSave(oldVersion){
+function fixOldSave(oldVersion) {
 }
